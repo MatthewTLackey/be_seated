b68f7747fbb8d5b5c8dd0bfea93cd2a77c0965ef
MatthewTLackey <lackey.matt@gmail.com>
2013-11-16 22:48:17 -0500

    Restaurant views and login working

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,28 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+   
+
+  end
+
+  def destroy
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeSeated::Application.config.session_store :cookie_store, key: '_be_seated_session'
